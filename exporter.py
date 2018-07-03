7178f8e11c40d95ad94672628b5de03e0e63ad64
hendrik lueth <hendrik.lueth@partner.kit.edu>
2018-07-03 16:00:24 +0200

    erste Version des exporters, bisher ohne tests

---
+++ exporter.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import json,requests
+from aruba_api_caller.aruba_api_caller import *
+from config import *
+from influxdb import InfluxDBClient
+
+apActivePerSwitch = {}
+radioDataPerSwitch = {}
+apData = {}
+
+MM = api_session(MM_IP, USER, PASSWORD, False)
+MM.login()
+switchList = MM.cli_command("show switches")
+apDatabase = MM.cli_command("show ap database")
+MM.logout()
+
+for switch in switchList["All Switches"]:
+  if switch["Model"] != "ArubaMM-VA" and switch["Status"] == "up":
+    tmpSession = api_session(switch["IP Address"], USER, PASSWORD, False)
+    tmpSession.login()
+    apActivePerSwitch[str(switch["IP Address"])] = tmpSession.cli_command("show ap active")
+    radioDataPerSwitch[str(switch["IP Address"])] = tmpSession.cli_command("show ap radio-summary")
+    tmpSession.logout()
+
+for ap in apDatabase["AP Database"]:
+  apData[ap["Name"]] = ap
+  apData[ap["Name"]]["11g Clients"] = 0
+  apData[ap["Name"]]["11a Clients"] = 0
+  apData[ap["Name"]]["Status"] = apData[ap["Name"]]["Status"].split(" ")[0]
+  apData[ap["Name"]]["Uptime"] = convertUptime(ap["Status"])
+  if apData[ap["Name"]]["Status"] == "Down":
+    apData[ap["Name"]]["Status_bin"] = 0
+  else:
+    apData[ap["Name"]]["Status_bin"] = 1 
+
+for switch in apActivePerSwitch:
+  for activeAP in apActivePerSwitch[switch]["Active AP Table"]:
+    apData[activeAP["Name"]]["11g Clients"] += int(activeAP["11g Clients"])
+    apData[activeAP["Name"]]["11a Clients"] += int(activeAP["11a Clients"])
+
+for switch in radioDataPerSwitch:
+  for thisRadio in radioDataPerSwitch[switch]["APs Radios information"]:
+    apData[thisRadio["Name"]]["Noise"+thisRadio["Band"]] = int(thisRadio["NF/U/I"].split("/")[0])
+    apData[thisRadio["Name"]]["Usage"+thisRadio["Band"]] = int(thisRadio["NF/U/I"].split("/")[1])
+    apData[thisRadio["Name"]]["Interference"+thisRadio["Band"]] = int(thisRadio["NF/U/I"].split("/")[2])
+
+
+json_body = []
+for Name in apData:
+  data = apData[Name]
+    json_body.append({
+                    "measurement": "AP_Data",
+                    "fields": data,
+                    "tags": {
+                      "host": data["Name"],
+                      "group":data["Group"],
+                      "ap_type":data["AP Type"],
+                      "status":data["Status"]
+                    }})
+
+InfluxClient = InfluxDBClient(InfluxIp, InfluxPort, InfluxUser, InfluxPassword, InfluxDbName)
+InfluxClient.write_points(json_body)
+
+
+#for x in (y for y in daten["RADIO Stats"] if y["Parameter"] == "Tx Data Frames  12 Mbps  (Mon)"):
+#  print (x["Value"])
