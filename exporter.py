77c7f0bd1813293a9f1cdd01dd6e782b0fe3f4a1
hendrik lueth <hendrik.lueth@partner.kit.edu>
2018-08-29 14:45:43 +0200

    adapt the script to the output of ArubaOS version 8.3.0.0

--- exporter.py
+++ exporter.py
@@ -26,10 +26,10 @@ for switch in switchList["All Switches"]:
     tmpSession = api_session(switch["IP Address"], USER, PASSWORD, False)
     tmpSession.login()
     if DEBUG:
-      print("requesting active APs from" + str(switch["IP Address"]))
+      print("requesting active APs from " + str(switch["IP Address"]))
     apActivePerSwitch[str(switch["IP Address"])] = tmpSession.cli_command("show ap active")
     if DEBUG:
-      print("requesting Radio-summary from" + str(switch["IP Address"]))
+      print("requesting Radio-summary from " + str(switch["IP Address"]))
     radioDataPerSwitch[str(switch["IP Address"])] = tmpSession.cli_command("show ap radio-summary")
     tmpSession.logout()
 
@@ -39,8 +39,8 @@ for ap in apDatabase["AP Database"]:
   apData[ap["Name"]] = ap
   apData[ap["Name"]]["11g Clients"] = 0
   apData[ap["Name"]]["11a Clients"] = 0
-  apData[ap["Name"]]["Status"] = apData[ap["Name"]]["Status"].split(" ")[0]
   apData[ap["Name"]]["Uptime"] = convertUptime(ap["Status"])
+  apData[ap["Name"]]["Status"] = apData[ap["Name"]]["Status"].split(" ")[0]
   if apData[ap["Name"]]["Status"] == "Down":
     apData[ap["Name"]]["Status_bin"] = 0
   else:
@@ -48,8 +48,41 @@ for ap in apDatabase["AP Database"]:
 
 for switch in apActivePerSwitch:
   for activeAP in apActivePerSwitch[switch]["Active AP Table"]:
-    apData[activeAP["Name"]]["11g Clients"] += int(activeAP["11g Clients"])
-    apData[activeAP["Name"]]["11a Clients"] += int(activeAP["11a Clients"])
+    #write the data for this radio to the dict
+    #if the radio does not exsist in this dict it is supposed to be None
+    if activeAP["Radio 0 Band Ch/EIRP/MaxEIRP/Clients"] is not None:
+      #this contains the Data, but splitted by radio-mode, Band and the Rest
+      tmpRadioData1 = activeAP["Radio 0 Band Ch/EIRP/MaxEIRP/Clients"].split(":")
+      #we take Channel, EIRP, MaxEIRP an Clients and split it by /
+      tmpRadioData2 = tmpRadioData1[2].split("/")
+      #we save the data to the dich
+      apData[activeAP["Name"]]["Radio0 channel"] = tmpRadioData2[0]
+      apData[activeAP["Name"]]["Radio0 EIRP"] = tmpRadioData2[1]
+      apData[activeAP["Name"]]["Radio0 MaxEIRP"] = tmpRadioData2[2]
+      #if the radio is 2.4GHz, we add the clients to the counter
+      if tmpRadioData[1].startswith("2.4"):
+        apData[activeAP["Name"]]["11g Clients"] += int(tmpRadioData2[3])
+      #if it is 5GHz we add it to the 5GHz Counter
+      else:
+        apData[activeAP["Name"]]["11a Clients"] += int(tmpRadioData2[3])
+    #write the data for this radio to the dict
+    #if the radio does not exsist in this dict it is supposed to be None
+    if activeAP["Radio 1 Band Ch/EIRP/MaxEIRP/Clients"] is not None:
+      #this contains the Data, but splitted by radio-mode, Band and the Rest
+      tmpRadioData1 = activeAP["Radio 0 Band Ch/EIRP/MaxEIRP/Clients"].split(":")
+      #we take Channel, EIRP, MaxEIRP an Clients and split it by /
+      tmpRadioData2 = tmpRadioData1[2].split("/")
+      #we save the data to the dich
+      apData[activeAP["Name"]]["Radio0 channel"] = tmpRadioData2[0]
+      apData[activeAP["Name"]]["Radio0 EIRP"] = tmpRadioData2[1]
+      apData[activeAP["Name"]]["Radio0 MaxEIRP"] = tmpRadioData2[2]
+      #if the radio is 2.4GHz, we add the clients to the counter
+      if tmpRadioData[1].startswith("2.4"):
+        apData[activeAP["Name"]]["11g Clients"] += int(tmpRadioData2[3])
+      #if it is 5GHz we add it to the 5GHz Counter
+      else:
+        apData[activeAP["Name"]]["11a Clients"] += int(tmpRadioData2[3])
+
 
 for switch in radioDataPerSwitch:
   for thisRadio in radioDataPerSwitch[switch]["APs Radios information"]:
